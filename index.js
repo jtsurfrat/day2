d86819d4dcd1b311faa48a111a7236f63e88165e
Jenna <jtsurfrat@hotmail.com>
2016-09-27 10:20:14 -0700

    first commit for day2

---
+++ index.js
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+  <title>Page Title</title>
+  <script src="scope.js" type="text/javascript">
+  </script>
+  </head>
+  <body>
+
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+    <script>
+
+
+  </body>
+</html>
